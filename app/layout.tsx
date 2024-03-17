d55122c902a169932a411f852476788831f042c2
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-03-17 04:30:09 +0100

    Major updates: dashboard/* /*

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import { inter } from '@/app/ui/fonts';
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +7,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <head>
+        <title>Next.js Dashboard - Okeke Divine-Vessel</title>
+      </head>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
