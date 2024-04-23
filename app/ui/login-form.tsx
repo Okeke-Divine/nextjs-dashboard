b1e368e2b2da22cec528ac7b1604578b87f677a3
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-04-23 13:25:19 +0100

    Added def values to login form

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -34,6 +34,7 @@ export default function LoginForm() {
                 id="email"
                 type="email"
                 name="email"
+                defaultValue={"user@nextmail.com"}
                 placeholder="Enter your email address"
                 required
               />
@@ -55,6 +56,7 @@ export default function LoginForm() {
                 name="password"
                 placeholder="Enter password"
                 required
+                defaultValue={"123456"}
                 minLength={6}
               />
               <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
